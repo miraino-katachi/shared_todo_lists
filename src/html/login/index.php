a71c2d7335711fd85d48d5375a7780ca7d1fcc21
坂上　正 <t_sakagami@miraino-katachih2.co.jp>
2020-02-13 10:34:55 +0900

    CSSの読み込み先をローカルに修正

--- src/html/login/index.php
+++ src/html/login/index.php
@@ -22,8 +22,7 @@ unset($_SESSION['post']);
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <title>ログイン</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="../css/main.css"> -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
 </head>
 
 <body>
