29b932033e3b6ba8b4279b6698e13c016e1de458
坂上　正 <t_sakagami@miraino-katachih2.co.jp>
2020-02-13 10:04:43 +0900

    CSSをBootstarp4 に変更

--- src/html/error/error.php
+++ src/html/error/error.php
@@ -1,41 +1,57 @@
 <!DOCTYPE html>
-<html>
+<html lang="ja">
+
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<title>エラー</title>
-<link rel="stylesheet" href="../css/normalize.css">
-<link rel="stylesheet" href="../css/main.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>ログイン</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
 </head>
+
 <body>
-<div class="container">
-    <header>
-         <div class="title">
-            <h1>エラー</h1>
+    <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+        <span class="navbar-brand">TODOリスト</span>
+    </nav>
+
+    <div class="container">
+        <div class="row my-2">
+            <div class="col-sm-3"></div>
+            <div class="col-sm-3">
+                <h1></h1>
+            </div>
+            <div class="col-sm-3"></div>
         </div>
-        <div class="login_info">
-            <ul>
-                <li></li>
-                <li>
-                    <form>
-                        <input type="button" value="ログアウト" onclick="location.href='../login/logout.php';">
-                    </form>
-                </li>
-            </ul>
+
+        <div class="row my-2">
+            <div class="col-sm-3"></div>
+            <div class="col-sm-6">
+                <!-- エラーメッセージ -->
+                <div class="row my-2">
+                    <div class="col-sm-3"></div>
+                    <div class="col-sm-6 alert alert-danger alert-dismissble fade show">
+                        申し訳ございません。<br>エラーが発生しました。
+                        <form class="mt-4">
+                            <input type="button" class="btn btn-danger" value="ログアウト"
+                                onclick="location.href='../login/';">
+                        </form>
+
+                    </div>
+                    <div class="col-sm-3"></div>
+                </div>
+                <!-- エラーメッセージ ここまで -->
+
+            </div>
+            <div class="col-sm-3"></div>
         </div>
-    </header>
 
-    <main>
-        <p class="error">
-            申し訳ございません。エラーが発生しました。
-        </p>
-        <form>
-            <input type="button" value="ログアウト" onclick="location.href='../login/logout.php';">
-        </form>
-    </main>
+    </div>
 
-    <footer>
+    <!-- 必要なJavascriptを読み込む -->
+    <script src="../js/jquery-3.4.1.min.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
 
-    </footer>
-</div>
 </body>
+
 </html>
